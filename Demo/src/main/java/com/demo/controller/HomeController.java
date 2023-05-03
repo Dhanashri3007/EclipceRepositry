eda66e515528a136f3456e0a43f0712e377d8e86
Dhanushree3007 <dhanu412admane@gmail.com>
2023-05-03 16:48:29 +0530

    controller update

--- Demo/src/main/java/com/demo/controller/HomeController.java
+++ Demo/src/main/java/com/demo/controller/HomeController.java
@@ -1,9 +1,12 @@
 package com.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class HomeController {
-@GetMapping("getData")
-public String getData()
-{
-  return "All Data";
-}
+	@GetMapping("getData")
+	public String getData() {
+		return "All Data";
+	}
 }
